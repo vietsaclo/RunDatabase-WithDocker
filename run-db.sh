a8dba4b235d5655d45fba3d585b8699ef8e25f28
vietsaclo <vietsaclo@gmail.com>
2024-03-28 22:30:05 +0700

    update code

--- run-db.sh
+++ run-db.sh
@@ -6,7 +6,7 @@ cp_prod="MY_docker-compose.prod.yml"
 read -p "Enter to select mode [dev/prod]: " MODE
 
 if [[ "$MODE" == "prod" ]]; then
-  sudo docker-compose --project-name app-test-please-replace-me --env-file .env -f db-compose/$cp_prod up -d --build
+  docker-compose --project-name app-test-please-replace-me --env-file .env -f db-compose/$cp_prod up -d --build
 else
-  sudo docker-compose --project-name app-test-please-replace-me --env-file .env -f db-compose/$cp_dev up -d --build
+  docker-compose --project-name app-test-please-replace-me --env-file .env -f db-compose/$cp_dev up -d --build
 fi
