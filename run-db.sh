a143a550e178492d1e7da5e199b15021502d3ace
vietsaclo <vietsaclo@gmail.com>
2024-03-28 21:49:16 +0700

    push code

---
+++ run-db.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cp_dev="MY_docker-compose.dev.yml"
+cp_prod="MY_docker-compose.prod.yml"
+
+read -p "Enter to select mode [dev/prod]: " MODE
+
+if [[ "$MODE" == "prod" ]]; then
+  sudo docker-compose --project-name app-test-please-replace-me --env-file .env -f db-compose/$cp_prod up -d --build
+else
+  sudo docker-compose --project-name app-test-please-replace-me --env-file .env -f db-compose/$cp_dev up -d --build
+fi
